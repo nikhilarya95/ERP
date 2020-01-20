b7ceb9921566b441841601527e42ae583a298012
nikhilarya95 <54001740+nikhilarya95@users.noreply.github.com>
2020-01-20 22:04:47 +0530

    Update slider.php
    .

--- application/views/KEC/slider.php
+++ application/views/KEC/slider.php
@@ -7,7 +7,7 @@
        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="http://localhost:8080/kec_erp/Assets/js/home.js" type="text/javascript"></script>
+  <script src="http://localhost/kec_erp/Assets/js/home.js" type="text/javascript"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
    
 </head>
@@ -23,15 +23,15 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
-        <img src="http://localhost:8080/kec_erp/application/img/mca.jpg" alt="MCA block" style="width:100%;height:360px;">
+        <img src="http://localhost/kec_erp/application/img/mca.jpg" alt="MCA block" style="width:100%;height:360px;">
       </div>
 
       <div class="item">
-        <img src="http://localhost:8080/kec_erp/application/img/adminb.jpg" alt="Admin block" style="width:100%;height:360px;">
+        <img src="http://localhost/kec_erp/application/img/adminb.jpg" alt="Admin block" style="width:100%;height:360px;">
       </div>
     
       <div class="item">
-        <img src="http://localhost:8080/kec_erp/application/img/itpark.jpg" alt="IT park" style="width:100%;height:360px;">
+        <img src="http://localhost/kec_erp/application/img/itpark.jpg" alt="IT park" style="width:100%;height:360px;">
       </div>
     </div>
 
